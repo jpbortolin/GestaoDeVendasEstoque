e65c7890777a2ca460a69b23db30bcd6cb5c04d8
João Pedro Bortolin <99106047+jpbortolin@users.noreply.github.com>
2025-03-06 18:15:53 -0300

    Update ConexaoMySQL.java

--- src/main/java/com/sellsync/gestaovenda/modelo/conexao/ConexaoMySQL.java
+++ src/main/java/com/sellsync/gestaovenda/modelo/conexao/ConexaoMySQL.java
@@ -8,7 +8,7 @@ public class ConexaoMySQL {
     
     private static final String URL = "jdbc:mysql://localhost:3306/gestaovenda";
     private static final String USER = "root";
-    private static final String PASSWORD = "root03";
+    private static final String PASSWORD = "root";
     
     private static Connection connection;
     
